c9963612f8b9607810795c062b29fbb597fd2231
ViShal69x <tk5762996@gmail.com>
2024-11-21 20:49:11 +0530

    fogos: include the proprietary version of boardconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,3 +31,6 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/properties/vendor.prop
 
 # Recovery
 TARGET_RECOVERY_UI_MARGIN_HEIGHT := 90
+
+# inherit from the proprietary version
+include vendor/motorola/fogos/BoardConfigVendor.mk
